6ca45fe53839abfc1ea208d293991c6d4d500999
dileesh chandra <dileeshchandra12@gmail.com>
2018-04-06 15:47:54 -0500

    Create stemming.py

---
+++ stemming.py
@@ -0,0 +1,12 @@
+# one more step of preprocessing
+# considers stemwords of a sentence, replaces with normal words
+#PorterStemmer is a stemming algorithm built into NLTK 
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+ps = PorterStemmer()
+example = ["slow","slowly", "slowed", "slowing", "slowesting"]
+
+for i in example:
+    print(ps.stem(i))
