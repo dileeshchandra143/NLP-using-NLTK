2467ece2922f6342589fa8bac14e4671914498c9
dileesh chandra <dileeshchandra12@gmail.com>
2018-04-08 17:33:54 -0500

    Update stopwords.py

--- stopwords.py
+++ stopwords.py
@@ -1,7 +1,7 @@
 # corpora and lexicon functionalities built into nltk used for preprocessing any body of text
-#corpora can be used for for any kind of speech in english or some other languages, we can create our own tokenizers based on corpora
-#Stop Words: the words which we need and computer dont need, like 'a', 'the', 'is' to filterout these words
-#the words which are not necessary for data analysis can be filtered by
+# corpora can be used for for any kind of speech in english or some other languages, we can create our own tokenizers based on corpora
+# Stop Words: the words which we need and computer dont need, like 'a', 'the', 'is' to filterout these words
+# the words which are not necessary for data analysis can be filtered by
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
